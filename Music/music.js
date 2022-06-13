927e3cb5afbcba4478c1646e41c9deaee671d5cf
isactes <fidel.renovato@gmail.com>
2022-06-13 15:02:25 -0500

    new

--- Music/music.js
+++ Music/music.js
@@ -101,7 +101,7 @@ playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
 function loadSong(song){
     title.textContent = song.displayName;
     artist.textContent = song.artist;
-    music.src = 'music/${song.name}.mp3';
+    music.src = `music/${song.name}.mp3`;
     image.src = `img/${song.name}.jpg`;
 }
 
