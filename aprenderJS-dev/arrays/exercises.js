dde7c99a313bdf39e1ea1742433a89e77bb30b5f
isactes <fidel.renovato@gmail.com>
2023-07-11 22:52:50 -0600

    add ejercicio de matrizes en js para buscar una en especifico

--- aprenderJS-dev/arrays/exercises.js
+++ aprenderJS-dev/arrays/exercises.js
@@ -141,3 +141,32 @@ console.log("Longer words:", result);
 console.log("Longer words:", resultFilterMap); 
 
 // NO SE POR QUE NO LO HAGGARA EN SU CONSOLOA DE MIDU DE LA PRUEBA Transformación de Arrays en JavaScript
+
+/*
+      Matrices
+
+      Recibimos una matriz de cadenas de texto. Tenemos que localizar la posición de la palabra "JavaScript" en la matriz y devolver su posición como un array de dos elementos: el índice de la fila y el índice de la columna.
+*/
+
+
+
+const matrix = [
+  ['HTML', 'CSS', 'JavaScript'],
+  ['Java', 'C++', 'Python'],
+  ['Ruby', 'Go', 'Swift']
+]
+
+function findJavaScript(matrix) {
+  // tu código aquí
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {    
+      if (matrix[i][j] === "JavaScript") {
+        return [i, j]
+      }
+    }
+  }
+  return [-1, -1]
+}
+
+const position = findJavaScript(matrix)
+console.log(position) // -> [0, 2]
