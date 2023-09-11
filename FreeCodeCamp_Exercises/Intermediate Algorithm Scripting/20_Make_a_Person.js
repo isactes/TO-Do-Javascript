a575070f20ea30a14a2c738d75c7ecef24e8ae51
isaclaleo <isac.renovato@librerialeo.com.mx>
2023-09-11 11:12:13 -0600

    ✨ new exercises freecodecamp

---
+++ FreeCodeCamp_Exercises/Intermediate Algorithm Scripting/20_Make_a_Person.js
@@ -0,0 +1,20 @@
+/*
+Make a Person
+Fill in the object constructor with the following methods below:
+
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(first, last)
+
+Run the tests to see the expected output for each method. These methods must be the only available means of interacting with the object. Each test will declare a new Person instance as new Person('Bob', 'Ross').
+*/
+
+const Person = function(first, last) {
+  this.getFullName = function() {
+    return "";
+  };
+  return "";
+};
