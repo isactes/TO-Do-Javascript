50239d0f24e3d9ad41a8a1231e544461fdfac8ce
isaclaleo <isac.renovato@librerialeo.com.mx>
2023-09-18 12:43:26 -0600

    freecodecamp roman numeral coverlar

--- FreeCodeCamp_Exercises/JavaScript Algorithms and Data Structures Projects/2_Roman_Numeral_Converter.js
+++ FreeCodeCamp_Exercises/JavaScript Algorithms and Data Structures Projects/2_Roman_Numeral_Converter.js
@@ -20,7 +20,37 @@ All roman numerals answers should be provided in upper-case.
 */
 
 function convertToRoman(num) {
-  return num;
+  // create roman numeral to number  looup table
+  const romanNumeral = {
+M:	1000,
+CM:	900,
+D:	500,
+CD:	400,
+C:	100,
+XC:	90,
+L:	50,
+XL:	40,
+X:	10,
+IX:	9,
+V:	5,
+IV:	4,
+I:	1
+  }
+
+  let accumalador = ''
+  for (const key in romanNumeral) {
+    const numberValue = romanNumeral[key];
+    console.log("⚒️", numberValue)
+
+    while(numberValue <= num){
+      num -= numberValue;
+      accumalador += key
+    }
+    
+  }
+
+  //  create numeral 
+  return accumalador;
  }
  
  console.log("✨",convertToRoman(36));
