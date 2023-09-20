1e0f985edc592110a8d59700db4f65a1b919c0a4
isaclaleo <isac.renovato@librerialeo.com.mx>
2023-09-20 11:03:48 -0600

    30 days of JS Day one

---
+++ 30_days_of_javascript/1_day/exercises/variable.js
@@ -0,0 +1,12 @@
+
+//  4 Crea un archivo variable.js y declarar variables y asignar tipos de datos string, booleanos, indefinidos y nulos
+
+let str = "Isactes"
+let bool = true
+let unde =  undefined
+let nul = null
+
+console.log("✨ ", str )
+console.log("✨ ", bool )
+console.log("✨ ", unde )
+console.log("✨ ", nul )
