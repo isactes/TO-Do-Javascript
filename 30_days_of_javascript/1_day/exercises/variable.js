d5ba0b58c2576f3095b274a45e10ff31f92b201b
isaclaleo <isac.renovato@librerialeo.com.mx>
2023-09-21 11:38:48 -0600

    Day 1 complete

--- 30_days_of_javascript/1_day/exercises/variable.js
+++ 30_days_of_javascript/1_day/exercises/variable.js
@@ -10,3 +10,54 @@ console.log("✨ ", str )
 console.log("✨ ", bool )
 console.log("✨ ", unde )
 console.log("✨ ", nul )
+
+// 5 Crea el archivo datatypes.js y utilizar el operador "typeof" de JavaScript para comprobar los diferentes tipos de datos. Comprueba el tipo de datos de cada variable
+
+let str1 = "Isactes"
+let bool1 = true
+let unde1 =  undefined
+let nul1 = null
+
+console.log("✨⚒️ ", typeof  str1 )
+console.log("✨ ", typeof bool1 )
+console.log("✨ ", typeof unde1 )
+console.log("✨ ", typeof nul1 )
+
+// 6 Declara cuatro variables sin asignar valores
+
+let name;
+let soal;
+let densi;
+let nose;
+
+//  7 Declara cuatro variables con valores asignados
+
+const age = 12
+const higth = 1.55
+const five = "isactes"
+const higt = 155
+
+// 8 Declara variables para almacenar su nombre, apellido, estado civil, país y edad en múltiples líneas
+
+const nombre1 = 'Asabeneh' // nombre de una persona
+const apellido = 'Yetayeh' // apellido de una persona
+const pais = 'Finland' // país
+const ciudad = 'Helsinki' // ciudad capital
+const edad1 = 100 // edad en años
+const estaCasado = true
+
+//  9 Declara variables para almacenar su nombre, apellido, estado civil, país y edad en una sola línea
+
+let nombre = 'Asabeneh', // nombre de una persona
+apellido2 = 'profesor',
+estadoCivil = 'Finlandia',
+pais2 = "CDMX",
+edad = 30
+
+//  10 Declara dos variables miEdad y tuEdad y asígneles valores iniciales y regístrese en la consola del navegador.
+
+const miEdad = 30
+const tuEdad = 22
+
+console.log("✨", "Tengo " + miEdad + " años")
+console.log("✨", "Tu tienes " + tuEdad + " años")
