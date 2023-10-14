46e8f4f4c640b2d383bb5f8dd2782f01d34846a0
isaclaleo <isac.renovato@librerialeo.com.mx>
2023-10-13 18:04:56 -0600

    30 days of JS day 2 exercises 1 to 6

---
+++ 30_days_of_javascript/2_day/index.js
@@ -0,0 +1,36 @@
+// Ejercicio: Nivel 1
+
+//  1. Declare una variable llamada desafío y asígnele un valor inicial '30 días de JavaScript'.
+
+var desafio = '30 dias de Javascript'
+
+//  2. Imprima la cadena en la consola del navegador usando console.log()
+
+const cadena = "la caneda en la consola"
+console.log("✨", cadena.length)
+
+//  3. Imprima la longitud de la cadena en la consola del navegador usando console.log()
+const cadena2 = "la caneda en la consola"
+console.log("✨", cadena2.length)
+
+//  4. Cambie todos los caracteres de cadena a letras mayúsculas usando el método toUpperCase()
+
+const cadena3 = "isactes"
+console.log("✨", cadena3.toUpperCase())
+
+// 5. Cambie todos los caracteres de la cadena a letras minúsculas usando el método toLowerCase()
+
+const cadena4 = "ISACTES"
+console.log("✨", cadena4.toLowerCase())
+
+//  6. Corta (segmenta) la primera palabra de la cadena usando el método substr() o substring()
+
+//  use substr
+const nameStr = "Isactes"
+console.log("✨", nameStr.substr(2,5))
+
+//  use substring
+const nameStr1 = "isactes"
+console.log("✨", nameStr1.substring(0,3))
+
+//  7. Corta la frase Days Of JavaScript de 30 Days Of JavaScript.
