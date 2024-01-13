75b36e0f00b511e49ee2f13b519fde2dcf577912
isaclaleo <isac.renovato@librerialeo.com.mx>
2024-01-12 18:07:16 -0600

    ✨ app some components it will be fix the preview and textArea

--- FreeCodeCamp_Exercises/Front_End_Development_Libraries/Build_a_Markdown_Previewer/src/App.jsx
+++ FreeCodeCamp_Exercises/Front_End_Development_Libraries/Build_a_Markdown_Previewer/src/App.jsx
@@ -1,17 +1,45 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
-import Example from "./components/example/example";
+import TitleOfProyect from "./components/titles/titleHone";
+import Head from "./components/head/Head";
 import TextArea from "./components/textArea/textArea";
+import Previewer from "./components/preview/Preview";
+import { sampleText } from "./sampleText";
 
 function App() {
+
+  const [ currenTab, setCurrenTab] = useState("editor")
+  const [ currentCode, setCurrenCode ] = useState(sampleText)
+
+  useEffect(() => {
+    console.log("✨", currenTab);
+    console.log("✨", currentCode);
+  }, [currenTab])
+
   return (
     <>
-        {/* <Example/> */}
-        <TextArea
-        text="Hola"
+      <TitleOfProyect
+      title="Build a Markdown Previewer"
+      />
+      <div className={``}>
+        <Head 
+        setTab={(tab) =>{
+          setCurrenTab(tab)
+        }}
         />
+        <div className="">
+          <div className={`${ currenTab !== 'editor' && 'hidden'} landscape:block landscape:w-[50vw]`}>
+            <TextArea 
+            setcode={(code) => {
+              setCurrenCode(code)
+            }}
+            />
+          </div>
+          <div className={`${ currenTab !== 'Previewer' && 'hidden'} landscape:block`}>
+            <Previewer code={currentCode}/>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
