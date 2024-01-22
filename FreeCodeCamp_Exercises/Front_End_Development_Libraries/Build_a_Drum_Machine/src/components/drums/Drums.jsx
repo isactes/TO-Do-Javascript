5f1249916a3d0093bc4b6c31f21683c9252c5f9c
isaclaleo <isac.renovato@librerialeo.com.mx>
2024-01-22 17:47:38 -0600

    ✨ add code logic

--- FreeCodeCamp_Exercises/Front_End_Development_Libraries/Build_a_Drum_Machine/src/components/drums/Drums.jsx
+++ FreeCodeCamp_Exercises/Front_End_Development_Libraries/Build_a_Drum_Machine/src/components/drums/Drums.jsx
@@ -1,20 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Sounds from "../../utilities/Sounds";
 import  "./Drums.css"
 function SoundDrumds() {
 
+  const [activeKey, setActiveKey ] = useState('')
+
+
   useEffect(() => {
     handleUseEffect()
   }, [])
-
-
 
 const handleUseEffect = () => {
   const useEffectHandle = document.addEventListener('keydown', (event) => {
     console.log('✨✨', event.key)
     handleOnclick(event.key) 
   })
-
   return useEffectHandle
 }
 
@@ -23,29 +23,30 @@ const handleOnclick = (selector) => {
   if(audio){
     console.log('✨', audio)
     audio.play();
+    setActiveKey(selector)
   }else{
     console.error('💀 error')
   }
 }
 
   return(
-    <div className="drum-pads">
+    <><div>{activeKey}</div><div className="drum-pads">
       {Sounds.map(drum => (
-        <button 
-        key={drum.src}
-        className="drum-pad" 
-        id={drum.src}
-        onClick={() => handleOnclick(drum.text)}
+        <button
+          key={drum.src}
+          className="drum-pad"
+          id={drum.src}
+          onClick={() => handleOnclick(drum.text)}
         >
           {drum.text}
           <audio
-          className="clip"
-          id={drum.text}
-          src={drum.src}
+            className="clip"
+            id={drum.text}
+            src={drum.src}
           ></audio>
         </button>
       ))}
-    </div>
+    </div></>
   )}
  
 export  default SoundDrumds
