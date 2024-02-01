a65f1962484a74e9ccb1540b1179a813ffbdbf0f
isactes <fidel.renovato@gmail.com>
2024-01-31 21:09:35 -0600

    ✨ drum machine done freecodecamp

--- FreeCodeCamp_Exercises/Front_End_Development_Libraries/Build_a_Drum_Machine/src/App.jsx
+++ FreeCodeCamp_Exercises/Front_End_Development_Libraries/Build_a_Drum_Machine/src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import SoundDrumds from './components/drums/Drums'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
