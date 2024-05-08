c771b0c85a66b47c05354f72c24b5fc81e4652e2
isaclaleo <isac.renovato@librerialeo.com.mx>
2024-05-08 17:57:56 -0600

    to completed more

--- FreeCodeCamp_Exercises/Front_End_Development_Libraries/Build_25_plus_5_Clock/src/main.jsx
+++ FreeCodeCamp_Exercises/Front_End_Development_Libraries/Build_25_plus_5_Clock/src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// import { Carousel, initTWE } from "tw-elements"
 import App from './App.jsx'
 import './index.css'
+
+// initTWE({ Carousel})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
